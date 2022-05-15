bb7c0e233aa960f8fa6d6892ab5880fb23d66a7b
Preent Loo <psilocybit5211@gmail.com>
2022-05-15 03:24:07 -0400

    patch_operations_structures_user.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from uuid import UUID
 # date
 from datetime import date, datetime
 # Optional
-from typing import Optional
+from typing import Optional, List
 #...E X T E R N A L
 # Pydantic
 from pydantic import BaseModel
@@ -13,6 +13,7 @@ from pydantic import EmailStr
 from pydantic import Field
 # FastAPI
 from fastapi import FastAPI
+from fastapi import status
 #==========FIN IMPORTS========#
 
 app = FastAPI();
@@ -62,8 +63,70 @@ class Tweet(BaseModel):
 
 
 #==========PATH OPERATIONS==========#
-# Path Operation del home page
+## Home Page
 @app.get(path="/")
 def home():
     return {"Twitter API": "Wor-king!"}
+
+## Users
+@app.post(
+    path = "/signup",
+    response_model = User,
+    status_code = status.HTTP_201_CREATED,
+    summary = "Register a user.",
+    tags = ["Users"]
+)
+def signup():
+    pass
+
+@app.post(
+    path = "/login",
+    response_model = User,
+    status_code = status.HTTP_200_OK,
+    summary = "Login a user.",
+    tags = ["Users"]
+)
+def login():
+    pass
+
+@app.get(
+    path = "/users",
+    response_model = List[User],
+    status_code = status.HTTP_200_OK,
+    summary = "Show all users.",
+    tags = ["Users"]
+)
+def show_all_users():
+    pass
+
+@app.get(
+    path = "/users/{user_id}",
+    response_model = User,
+    status_code = status.HTTP_200_OK,
+    summary = "Show a user.",
+    tags = ["Users"]
+)
+def show_a_user():
+    pass
+
+@app.delete(
+    path = "/users/{user_id}/delete",
+    response_model = User,
+    status_code = status.HTTP_200_OK,
+    summary = "Delete a user.",
+    tags = ["Users"]
+)
+def delete_a_user():
+    pass
+
+@app.put(
+    path = "/users/{user_id}/update",
+    response_model = User,
+    status_code = status.HTTP_200_OK,
+    summary = "Update a user.",
+    tags = ["Users"]
+)
+def update_a_user():
+    pass
+## Tweets
 #=========FIN PATH OPERATIONS========#
